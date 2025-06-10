71cb2913310aaf7804bf0e5fa1962fc6b7b83a95
Phatra-Vip <Phatra.won@student.mahidol.ac.th>
2025-06-10 19:52:10 +0700

    Fix error ver 3

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,8 +2,8 @@
 
 import axios from 'axios';
 
-// const API_BASE_URL = 'https://vipstore-backend.onrender.com/api';
-const API_BASE_URL = process.env.VITE_API_URL || 'http://localhost:3001/api';
+const API_BASE_URL = 'https://vipstore-backend.onrender.com/api';
+// const API_BASE_URL = process.env.VITE_API_URL || 'http://localhost:3001/api';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
